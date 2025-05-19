46da72408f2603c30f2a638eee2f6a95a98b7946
duddk <duddk@scatterx.io>
2025-05-19 10:16:32 +0900

    제품 상세 페이지 추가

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,4 +1,3 @@
 export default function Button({ text, onClick }) {
   return <button className='cursor-pointer text-white py-2 px-4 rounded-sm hover:brightness-110' style={{background: 'var(--color-brand)'}} onClick={onClick}>{text}</button>
-  
 }
