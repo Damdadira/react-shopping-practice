fb37b468e184959f23c7f166865bba6c0086ca81
duddk <duddk@scatterx.io>
2025-05-20 09:37:48 +0900

    장바구니 추가 성공 시 메시지 출력

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -10,13 +10,19 @@ export default function ProductDetail() {
       product: { id, image, title, description, category, price, options }
     }
   } = useLocation();
+  const [success, setSuccess] = useState();
   const [selected, setSelected] = useState(options && options[0]);
 
   const handleSelect = (e) => setSelected(e.target.value);
   const handleClick = (e) => {
     // 여기서 장바구니에 추가해주면 됨!
     const product = { id, image, title, price, option: selected, quantity: 1 };
-    addOrUpdateItem.mutate(product);
+    addOrUpdateItem.mutate(product, {
+      onSuccess: () => {
+        setSuccess('장바구니에 추가되었습니다.');
+        setTimeout(() => setSuccess(null), 3000);
+      }
+    });
   }
 
   return (
@@ -38,6 +44,7 @@ export default function ProductDetail() {
               ))}
             </select>
           </div>
+          {success && <p className='my-2'>{success}</p>}
           <Button text='장바구니에 추가' onClick={handleClick}></Button>
         </div>
       </section>
