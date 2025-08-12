1fedf5ac44d46d2a0b4bebe7bb0c4057fc802233
duddk <duddk@scatterx.io>
2025-08-12 16:06:17 +0900

    style: banner 이미지 주소 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,7 @@
 export default function Banner() {
   return (
     <section className='h-96 bg-yellow-900 relative'>
-      <div className='w-full h-full opacity-80' style={{backgroundImage: 'url(../public/images/banner.png)'}}></div>
+      <div className='w-full h-full opacity-80' style={{backgroundImage: 'url(/public/images/banner.png)'}}></div>
       <div className='absolute w-full top-36 text-center text-gray-50 drop-shadow-2xl'>
         <h2 className='text-6xl pb-4'>Shop With US</h2>
         <p className='text-2xl'>Best Products, High Quality</p>
