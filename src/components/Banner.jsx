4c73c4d25e5cb236bb9251ebff054a1669267df4
duddk <duddk@scatterx.io>
2025-08-13 10:19:31 +0900

    feat: readme 이미지 추가

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <section className='h-96 bg-yellow-900 relative'>
+    <section className='h-96 relative'>
       <div className='w-full h-full opacity-80' style={{backgroundImage: 'url(/images/banner.png)'}}></div>
       <div className='absolute w-full top-36 text-center text-gray-50 drop-shadow-2xl'>
         <h2 className='text-6xl pb-4'>Shop With US</h2>
